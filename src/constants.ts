cf6fa346fe6641e512d4643810c518ec01a17584
张 泽蒙 <zhangzemeng@travelsky.com>
2020-11-15 12:05:58 +0800

    增加对于未知结构的优先级声明

--- src/constants.ts
+++ src/constants.ts
@@ -12,9 +12,11 @@ export const CREEP_TYPE = {
   repairer: 'repairer',
 }
 
-// 存放能量的优先级, 数字越小，优先级越高
+// 能量结构存放的优先级, 数字越小，优先级越高
 export const STORAGE_PRIORITY = {
   [STRUCTURE_SPAWN]: 100,
   [STRUCTURE_EXTENSION]: 200,
-  [STRUCTURE_CONTAINER]: 300
+  [STRUCTURE_CONTAINER]: 300,
+  // 未知的结构
+  unknown: 999
 }
